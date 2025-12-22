ad781077ba6e521010523f687ac496e504ff667b
Deesster <deesssstt@gmail.com>
2025-12-22 16:36:13 +0200

    Add basic task manager functionality

--- task_manager.py
+++ task_manager.py
@@ -1,1 +1,26 @@
-print("lowkey")
+tasks=[]
+while True:
+  print("\nTask Manager")
+  print("1. Add task")
+  print("2. Show task")
+  print("3. Exit")
+  choise=input("Choose an option (1-3):")
+  if choise =="1":
+    task=input("Enter a new task:")
+    tasks.append(task)
+    print("Task added")
+  elif choise =="2":
+    if not tasks:
+      print("No tasks yet")
+    else:
+      print ("\nYour tasks:")
+      for i, task in enumerate(tasks, start=1):
+        print(f"{i}. {task}")
+  elif choise =="3":
+    print("byyee")
+    break
+  else:
+    print("Invalid choise. Try aagain")
+    
+      
+  
