1dfdb87242b57498153740d83a62b12d3415cca9
Deesster <deesssstt@gmail.com>
2025-12-26 02:56:47 +0200

    Update task_manager.py

--- task_manager.py
+++ task_manager.py
@@ -3,7 +3,8 @@ while True:
   print("\nTask Manager")
   print("1. Add task")
   print("2. Show task")
-  print("3. Exit")
+  print("3.Delete task")
+  print("4. Exit")
   choise=input("Choose an option (1-3):")
   if choise =="1":
     task=input("Enter a new task:")
@@ -17,10 +18,20 @@ while True:
       for i, task in enumerate(tasks, start=1):
         print(f"{i}. {task}")
   elif choise =="3":
+    if not tasks:
+      print("No tasks to delete")
+    else:
+      for i, task in enumerate(tasks, start=1):
+        print(f"{i}. {task}")
+      num=int(input("Enter task number to delete:"))
+      if 1<=num<=len(tasks):
+        tasks.pop(num-1)
+        print("Task deletet!")
+      else:
+        print ("Invalid number")
+  elif choise== "4":
     print("byyee")
     break
-  else:
-    print("Invalid choise. Try aagain")
     
       
   
